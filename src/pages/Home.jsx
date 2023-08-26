f47449500f94489d9883fd645e70a8fb090ef83c
Nandan G N <67967990+gnnandan@users.noreply.github.com>
2023-08-26 22:56:09 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="text-center">
       <Navbar/>
       <h1 className= "mt-10 text-2xl">Home</h1>
-      <h2>Sample Git Push</h2>
+      <h2>Curious Developers Community</h2>
       <Form/>
     </div>
   )
